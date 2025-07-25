99327f773b0e007d422fd7ed0965e2352e431f66
Charlie <charlie@Charlies-Mac-mini.local>
2025-07-25 02:36:54 -0700

    Logout Warning

--- src/App.tsx
+++ src/App.tsx
@@ -269,28 +269,37 @@ for (const id of tokenIds) {
   Profile
   <img src={profile.pfp_url} alt="pfp" className="pfp-inline" />
 </button>
-<button
-        className="my-towers-button logout-button"
-        onClick={() => disconnect()}
-        title="Disconnect Wallet"
-      >
-        {/* Logout icon (door with arrow) */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="#00B3FF"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
-          <polyline points="10 17 15 12 10 7" />
-          <line x1="15" y1="12" x2="3" y2="12" />
-        </svg>
-      </button>
+<div className="tooltip-container">
+  <button
+    className="my-towers-button logout-button"
+    onClick={() => disconnect()}
+    onMouseEnter={() => {
+      const tooltip = document.getElementById('logout-tooltip');
+      if (tooltip) tooltip.style.opacity = '1';
+    }}
+    onMouseLeave={() => {
+      const tooltip = document.getElementById('logout-tooltip');
+      if (tooltip) tooltip.style.opacity = '0';
+    }}
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
+      <polyline points="10 17 15 12 10 7" />
+      <line x1="15" y1="12" x2="3" y2="12" />
+    </svg>
+  </button>
+  <div id="logout-tooltip" className="tooltip-text">Are you sure you want to log out?</div>
+</div>
 
     
     </>
