8d992b2a3205bf712857348e2af9cd8cfa194777
Charlie <charlie@Charlies-Mac-mini.local>
2025-07-27 10:37:56 -0700

    Change VW on My Towers

--- src/components/MyTowersModal.tsx
+++ src/components/MyTowersModal.tsx
@@ -76,7 +76,7 @@ export default function MyTowersModal({ walletAddress, onClose }: Props) {
   id="profile-card"
   style={{
     width: '90%',
-    maxWidth: towers.length === 0 ? '600px' : '70vw',
+    maxWidth: towers.length === 0 ? '500px' : '50vw',
     padding: '20px',
     background: '#0D1117',
     borderRadius: '16px',
